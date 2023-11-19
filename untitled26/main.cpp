2c613065167b0f2fc5dae2b15b27ad40fc36aba1
Enemy89 <66246104+Enemy89@users.noreply.github.com>
2023-11-19 10:52:04 +0300

    Add files via upload

--- untitled26/main.cpp
+++ untitled26/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+class Monitor {
+    int height = 50;
+    int width = 80;
+
+public:
+    int getHeightM () {
+        return height;
+    }
+
+    int getWidthM () {
+        return width;
+    }
+};
+
 class Window{
     int leftCornerX=3;
     int leftCornerY=25;
@@ -23,33 +37,33 @@ public:
         return width;
     }
 
-    void move (int valueW, int valueH) {
+    void move (int valueW, int valueH, Monitor& monitor) {
         leftCornerX=0;
         leftCornerY=0;
         if (leftCornerY+valueH<0) leftCornerY=0;
-        if (leftCornerY+valueH>50) leftCornerY=50;
+        if (leftCornerY+valueH>monitor.getHeightM()) leftCornerY=50;
         if (leftCornerX+valueW<0) leftCornerX=0;
-        if (leftCornerX+valueW>80) leftCornerX=80;
-        if (leftCornerY+valueH>=0 && leftCornerY+valueH<=50) leftCornerY+=valueH;
-        if (leftCornerX+valueW>=0 && leftCornerX+valueW<=80) leftCornerX+=valueW;
+        if (leftCornerX+valueW>monitor.getWidthM()) leftCornerX=80;
+        if (leftCornerY+valueH>=0 && leftCornerY+valueH<=monitor.getHeightM()) leftCornerY+=valueH;
+        if (leftCornerX+valueW>=0 && leftCornerX+valueW<=monitor.getWidthM()) leftCornerX+=valueW;
         std::cout<<"Coordinate window: "<<getLeftCornerX()<<";"<<getRightCornerY()<<std::endl;
     }
 
-    void resize (int widthX, int heightY) {
+    void resize (int widthX, int heightY, Monitor& monitor) {
         width=0;
         height=0;
         if (width+widthX<0) width=0;
         if (height+heightY<0) height=0;
-        if (width+widthX>80) width=80;
-        if (height+heightY>50) height=50;
-        if (width+widthX>=0 && width+widthX<=80) width+=widthX;
-        if (height+heightY>=0 && height+heightY<=50) height+=heightY;
+        if (width+widthX>monitor.getWidthM()) width=80;
+        if (height+heightY>monitor.getHeightM()) height=50;
+        if (width+widthX>=0 && width+widthX<=monitor.getWidthM()) width+=widthX;
+        if (height+heightY>=0 && height+heightY<=monitor.getHeightM()) height+=heightY;
         std::cout<<"New size: width "<<gatWidthW()<<" height "<<getHeightW()<<std::endl;
     }
 
-    void display () {
-        for (int y = 50; y>0; --y) {
-            for (int x = 0; x < 80; ++x) {
+    void display (Monitor& monitor) {
+        for (int y = monitor.getHeightM(); y>0; --y) {
+            for (int x = 0; x < monitor.getWidthM(); ++x) {
                 if (x>=leftCornerX-1 && x<leftCornerX+width && y<=leftCornerY && y>leftCornerY-height) {
                     std::cout << " 1 ";
                 } else {
@@ -61,50 +75,31 @@ public:
     }
 };
 
-class Monitor {
-    int height = 50;
-    int width = 80;
-
-public:
-    int getHeightM () {
-        return height;
-    }
-
-    int getWidthM () {
-        return width;
-    }
-};
-
 int main() {
     Monitor *monitor = new Monitor;
-    Window * window = new Window;
+    Window *window = new Window;
     while (true) {
         std::cout << "Enter the command (move/resize/display/close):" << std::endl;
         std::string command;
         std::cin >> command;
 
-        if (command=="close"){
+        if (command == "close") {
             break;
-        } else {
-            if (command=="move") {
-                std::cout<<"Enter coordinate: "<<std::endl;
-                int x,y;
-                std::cin>>x>>y;
-                window->move(x,y);
-            } else {
-                if (command=="resize") {
-                    std::cout<<"Enter new size: "<<std::endl;
-                    int width, height;
-                    std::cin>>width>>height;
-                    window->resize(width,height);
-                } else {
-                    if (command=="display") {
-                        window->display();
-                    }
-                }
-            }
+        } else if (command == "move") {
+            std::cout << "Enter coordinate: " << std::endl;
+            int x, y;
+            std::cin >> x >> y;
+            window->move(x, y, *monitor);
+        } else if (command == "resize") {
+            std::cout << "Enter new size: " << std::endl;
+            int width, height;
+            std::cin >> width >> height;
+            window->resize(width, height, *monitor);
+        } else if (command == "display") {
+            window->display(*monitor);
         }
     }
+
     delete window;
     delete monitor;
 }
